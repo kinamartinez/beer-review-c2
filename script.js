476ba7421cd5d730879044efe95b8c56119c86d1
Karina Martinez <kinamartinez@gmail.com>
2017-02-16 11:53:31 +0200

    make some changes to present the code modular

--- script.js
+++ script.js
@@ -1,32 +1,49 @@
-var beers = [];
 
+
+function BeerReviewApp() {
+    var beers = [];
+
+
+    var addBeer = function (beerName, category, rating) {
+        beers.push({
+            beerName: beerName,
+            category: category,
+            rating: rating
+        });
+
+    };
+        var updateBeers = function () {
+            $(".beers-list").find("li").remove();
+            for (var i = 0; i < beers.length; i++) {
+                $(".beers-list").append("<li>"+"Name" + " " + beers[i]["beerName"] + " - " + "Category" + " " + beers[i]["category"] + " - " + "Rating" + " " + beers[i]["rating"]+"</li>");
+
+            }
+        };
+
+
+    return {
+        updateBeers: updateBeers,
+        addBeer: addBeer,
+        beers: beers
+    }
+
+
+}
+var BeerReviewApp = BeerReviewApp ();
 
 $(".post-beer").on("click", function () {
     var beerName = $(".beer-input").val();
     var category = $(".category-input").val();
     var rating = $(".rating-input").val();
 
-    addBeer(beerName, category, rating);
-    updateBeers();
-    sortByKey(beers);
+    BeerReviewApp.addBeer(beerName, category, rating);
+
+    BeerReviewApp.updateBeers();
+
+
 });
 
-var addBeer = function (beerName, category, rating) {
-    beers.push({
-        beerName: beerName,
-        category: category,
-        rating: rating
-    });
 
-
-};
-var updateBeers = function () {
-    $(".beers-list").find("li").remove();
-    for (var i = 0; i < beers.length; i++) {
-        $(".beers-list").append("<li></li>");
-        $(".beers-list li:last-child").append("Name" + " " + beers[i]["beerName"] + " - " + "Category" + " " + beers[i]["category"] + " - " + "Rating" + " " + beers[i]["rating"]);
-    }
-};
 
 function sortByKey() {
     beers.sort(function (a, b) {
@@ -40,7 +57,7 @@ function sortByKeyReverse() {
     })
 }
 
-console.log(beers);
+console.log(BeerReviewApp.beers);
 
 
 
